dff9f8fc91ac202af4397ba3bccd11f3394c98e3
andrewmoren <andrewmoren93@gmail.com>
2022-12-10 01:56:53 +0300

    add pages && components + routers

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -1,7 +1,5 @@
 import React from "react";
-
 import { Routes, Route } from "react-router-dom";
-
 import Home from "../pages/Home";
 import Cart from "../pages/Cart";
 import Checkout from "../pages/Checkout";
