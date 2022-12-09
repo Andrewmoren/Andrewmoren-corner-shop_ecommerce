80a6ad7167fda7b396e9df020208c77fbb83c98f
andrewmoren <andrewmoren93@gmail.com>
2022-12-10 01:54:23 +0300

    first commit

---
+++ src/routers/Routers.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Cart from "../pages/Cart";
+import Checkout from "../pages/Checkout";
+import Shop from "../pages/Shop";
+import Login from "../pages/Login";
+import ProductDetails from "../pages/ProductDetails";
+import Signup from "../pages/Signup";
+
+const Routers = () => {
+  return (
+    <Routes>
+      <Route path="home" element={<Home />} />
+      <Route path="shop" element={<Shop />} />
+      <Route path="shop/:id" element={<ProductDetails />} />
+      <Route path="cart" element={<Cart />} />
+      <Route path="checkout" element={<Checkout />} />
+      <Route path="login" element={<Login />} />
+      <Route path="signup" element={<Signup />} />
+    </Routes>
+  );
+};
+
+export default Routers;
