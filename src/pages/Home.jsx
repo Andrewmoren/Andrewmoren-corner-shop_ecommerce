d3a9b8f9733c0c1477e460200c9a7fb8bf7d56d3
andrewmoren <andrewmoren93@gmail.com>
2022-12-13 19:04:46 +0300

    build clock-timer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,7 @@ import { Container, Row, Col } from "reactstrap";
 
 import heroImg from "../assets/images/hero-img.png";
 import counterImg from "../assets/images/counter-timer-img.png";
+import Clock from "../components/UI/Clock";
 import { Link } from "react-router-dom";
 
 import Services from "../services/Services";
@@ -89,10 +90,23 @@ const Home = () => {
       <section className="timer__count">
         <Container>
           <Row>
-            <Col lg="6" md="6"></Col>
-
             <Col lg="6" md="6">
-              <img src={counterImg} alt="" />
+              <div className="clock__top-content">
+                <h4 className="text-white fs-6 mb-2">Limited Offers</h4>
+                <h3 className="text-white fs-5 mb-3">Quality Armchair</h3>
+              </div>
+              <Clock />
+
+              <motion.button
+                whileTap={{ scale: 1.2 }}
+                className="buy__btn store__btn"
+              >
+                <Link to="/shop">Visit Store</Link>
+              </motion.button>
+            </Col>
+
+            <Col lg="6" md="6" className="text-end">
+              <img src={counterImg} alt="counter" />
             </Col>
           </Row>
         </Container>
