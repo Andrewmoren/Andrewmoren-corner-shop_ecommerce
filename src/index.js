a6ebb4bba197001b49c1353a54353b2e532a0689
andrewmoren <andrewmoren93@gmail.com>
2022-12-15 00:13:11 +0300

    add redux toolkit

--- src/index.js
+++ src/index.js
@@ -7,11 +7,16 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
+import store from "./redux/store";
+import { Provider } from "react-redux";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
