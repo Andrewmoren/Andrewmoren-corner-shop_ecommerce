79adef54f751c640fe8efae48f57ced61602fe35
andrewmoren <andrewmoren93@gmail.com>
2022-12-16 20:38:02 +0300

    create shop: logic filtred and searching

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,7 +1,118 @@
-import React from "react";
+import React, { useState } from "react";
+import CommonSection from "../components/UI/CommonSection";
+import Helmet from "../components/Helmet/Helmet";
+import { Container, Row, Col } from "reactstrap";
+import "../styles/shop.css";
+
+import products from "../assets/data/products";
+import ProductList from "../components/UI/ProductsList";
 
 const Shop = () => {
-  return <div>Shop</div>;
+  const [productData, setProductData] = useState(products);
+
+  const handleFilter = (e) => {
+    const filterValue = e.target.value;
+    if (filterValue === "sofa") {
+      const filterProducts = products.filter(
+        (item) => item.category === "sofa"
+      );
+      setProductData(filterProducts);
+    }
+
+    if (filterValue === "mobile") {
+      const filterProducts = products.filter(
+        (item) => item.category === "mobile"
+      );
+      setProductData(filterProducts);
+    }
+
+    if (filterValue === "chair") {
+      const filterProducts = products.filter(
+        (item) => item.category === "chair"
+      );
+      setProductData(filterProducts);
+    }
+
+    if (filterValue === "watch") {
+      const filterProducts = products.filter(
+        (item) => item.category === "watch"
+      );
+      setProductData(filterProducts);
+    }
+
+    if (filterValue === "wireless") {
+      const filterProducts = products.filter(
+        (item) => item.category === "wireless"
+      );
+      setProductData(filterProducts);
+    }
+  };
+
+  const handleSearch = (e) => {
+    const searchTerm = e.target.value;
+
+    const searchedProducts = products.filter((item) =>
+      item.productName.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setProductData(searchedProducts);
+  };
+
+  return (
+    <Helmet title="Shop">
+      <CommonSection title="Products" />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="3" md="3">
+              <div className="filter__widget">
+                <select onChange={handleFilter}>
+                  <option>Filter By Category</option>
+                  <option value="sofa">Sofa</option>
+                  <option value="mobile">Mobile</option>
+                  <option value="chair">Chair</option>
+                  <option value="watch">Watch</option>
+                  <option value="wireless">Wireless</option>
+                </select>
+              </div>
+            </Col>
+            <Col lg="3" md="3">
+              <div className="filter__widget">
+                <select>
+                  <option>Sort By</option>
+                  <option value="ascending">Ascending</option>
+                  <option value="descending">Descending</option>
+                </select>
+              </div>
+            </Col>
+            <Col lg="6" md="6">
+              <div className="search__box">
+                <input
+                  type="text"
+                  placeholder="Search....."
+                  onChange={handleSearch}
+                />
+                <span>
+                  <i className="ri-search-line"></i>
+                </span>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <section className="pt-0">
+        <Container>
+          <Row>
+            {productData.length === 0 ? (
+              <h1 className="text-center fs-4">No products are found!</h1>
+            ) : (
+              <ProductList data={productData} />
+            )}
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default Shop;
