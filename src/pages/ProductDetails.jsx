66beaece1a42b519aac566d61ce6dab9e0816b0b
andrewmoren <andrewmoren93@gmail.com>
2022-12-17 01:57:47 +0300

    create product details

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
+import { Container, Row, Col } from "reactstrap";
+import { useParams } from "react-router-dom";
+import products from "../assets/data/products";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/CommonSection";
+
 const ProductDetails = () => {
-  return <div>ProductDetails</div>;
+  const { id } = useParams();
+  const product = products.find((item) => item.id === id);
+  const { imgUrl, productName, price, avgRating, review, description } =
+    product;
+  return (
+    <Helmet>
+      <CommonSection />
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6"></Col>
+            <Col lg="6"></Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default ProductDetails;
