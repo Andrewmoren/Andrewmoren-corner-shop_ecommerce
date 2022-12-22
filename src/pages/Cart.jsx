09f53bca0e83951892e1a343d871647b59252816
andrewmoren <andrewmoren93@gmail.com>
2022-12-22 13:56:54 +0300

    create deleteItems in redix & fixed cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -7,9 +7,11 @@ import { cartActions } from "../redux/slices/cartSlice";
 import { useSelector, useDispatch } from "react-redux";
 
 import "../styles/cart.css";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
   const cartItems = useSelector((state) => state.cart.cartItems);
+  const totalAmount = useSelector((state) => state.cart.totalAmount);
   return (
     <Helmet title="Cart">
       <CommonSection title="Shoping Cart" />
@@ -32,31 +34,62 @@ const Cart = () => {
                   </thead>
 
                   <tbody>
-                    {cartItems.map((items, index) => (
-                      <tr key={index}>
-                        <td>
-                          <img src={items.imgUrl} alt="image" />
-                        </td>
-                        <td>{items.productName}</td>
-                        <td>$ {items.price}</td>
-                        <td>{items.quantity}px</td>
-                        <td>
-                          <motion.i
-                            whileTap={{ scale: 1.1 }}
-                            className="ri-delete-bin-line"
-                          ></motion.i>
-                        </td>
-                      </tr>
+                    {cartItems.map((item, index) => (
+                      <Tr item={item} key={index} />
                     ))}
                   </tbody>
                 </table>
               )}
             </Col>
-            <Col lg="9"></Col>
+            <Col lg="3">
+              <div>
+                <h6 className="d-flex align-items-center justify-content-between">
+                  Subtotal
+                  <span className="fs-4 fw-bold">${totalAmount}</span>
+                </h6>
+              </div>
+              <p className="fs-6 mt-2">
+                taxes and shipping will calculate in checkout
+              </p>
+              <div>
+                <button className="buy__btn w-100 ">
+                  <Link to="/checkout">Checkout</Link>
+                </button>
+
+                <button className="buy__btn w-100 mt-3">
+                  <Link to="/shop">Continue Shopping</Link>
+                </button>
+              </div>
+            </Col>
           </Row>
         </Container>
       </section>
     </Helmet>
+  );
+};
+
+const Tr = ({ item }) => {
+  const dispatch = useDispatch();
+
+  const deleteProduct = () => {
+    dispatch(cartActions.deleteItem(item.id));
+  };
+  return (
+    <tr>
+      <td>
+        <img src={item.imgUrl} alt="image" />
+      </td>
+      <td>{item.productName}</td>
+      <td>$ {item.price}</td>
+      <td>{item.quantity}px</td>
+      <td>
+        <motion.i
+          whileTap={{ scale: 1.1 }}
+          onClick={deleteProduct}
+          className="ri-delete-bin-line"
+        ></motion.i>
+      </td>
+    </tr>
   );
 };
 
--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -34,7 +34,22 @@ const cartSlice = createSlice({
       }
 
       state.totalAmount = state.cartItems.reduce(
-        (total, item) => total + Number(item.price) * Number(item.quantity)
+        (total, item) => total + Number(item.price) * Number(item.quantity),
+        0
+      );
+    },
+    deleteItem: (state, action) => {
+      const id = action.payload;
+      const existingItem = state.cartItems.find((item) => item.id === id);
+
+      if (existingItem) {
+        state.cartItems = state.cartItems.filter((item) => item.id !== id);
+        state.totalQuantity = state.totalQuantity - existingItem.quantity;
+      }
+
+      state.totalAmount = state.cartItems.reduce(
+        (total, item) => total + Number(item.price) * Number(item.quantity),
+        0
       );
     },
   },
