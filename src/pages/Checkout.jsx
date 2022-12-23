b3efee7b7d06c064106224fb927cbc571da6a774
andrewmoren <andrewmoren93@gmail.com>
2022-12-23 23:37:51 +0300

    create checkout component

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Container, Row, Col, Form, FormGroup } from "reactstrap";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/CommonSection";
 
 const Checkout = () => {
-  return <div>Checkout</div>;
+  return (
+    <Helmet title="Checkout">
+      <CommonSection title="Checkout" />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="8">
+              <h6>Information</h6>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default Checkout;
