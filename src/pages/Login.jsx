a8b178b5b1afd38d42315be0714613ba2c38569c
andrewmoren <andrewmoren93@gmail.com>
2022-12-26 14:52:19 +0300

    create login component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import Helmet from "../components/Helmet/Helmet";
+import { Container, Row, Col, Form, FormGroup } from "reactstrap";
+import { Link } from "react-router-dom";
+
+import "../styles/login.css";
+import { useState } from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <Helmet title="Login">
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" className="m-auto text-center">
+              <h3 className="fw-bold mb-4">Login</h3>
+              <Form className="auth__form">
+                <FormGroup className="form__group">
+                  <input type="email" placeholder="Enter you email" />
+                </FormGroup>
+                <FormGroup className="form__group">
+                  <input type="password" placeholder="Enter you password" />
+                </FormGroup>
+
+                <button className="buy__btn auth__btn">Login</button>
+                <p>
+                  Don't have an account?
+                  <Link to="/signup">Create an account</Link>
+                </p>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default Login;
