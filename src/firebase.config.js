2ae72742516e77419773494c1c47fca35125fb3c
andrewmoren <andrewmoren93@gmail.com>
2022-12-26 15:16:54 +0300

    add firebase config

---
+++ src/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCUicGJ1b3SJdHl3u_y5D5MXZ6AnpzhCVo",
+  authDomain: "cornershop-bab94.firebaseapp.com",
+  projectId: "cornershop-bab94",
+  storageBucket: "cornershop-bab94.appspot.com",
+  messagingSenderId: "788586197934",
+  appId: "1:788586197934:web:9e192a3a34868795700e3f",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
