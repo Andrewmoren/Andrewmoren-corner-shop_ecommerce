285cf06288ee688a491aa1cf25e5491b80a520fd
andrewmoren <andrewmoren93@gmail.com>
2022-12-27 15:44:07 +0300

    fix login component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Helmet from "../components/Helmet/Helmet";
 import { Container, Row, Col, Form, FormGroup } from "reactstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase.config";
 import { toast } from "react-toastify";
@@ -12,6 +12,7 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   const signIn = async (e) => {
     e.preventDefault();
@@ -25,6 +26,9 @@ const Login = () => {
       const user = userCredential.user;
 
       console.log(user);
+      setLoading(false);
+      toast.success("Successfully logged in");
+      navigate("/checkout");
     } catch (error) {
       setLoading(false);
       toast.success(error.message);
@@ -35,35 +39,41 @@ const Login = () => {
       <section>
         <Container>
           <Row>
-            <Col lg="6" className="m-auto text-center">
-              <h3 className="fw-bold mb-4">Login</h3>
-              <Form className="auth__form">
-                <FormGroup className="form__group">
-                  <input
-                    type="email"
-                    placeholder="Enter you email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </FormGroup>
-                <FormGroup className="form__group">
-                  <input
-                    type="password"
-                    placeholder="Enter you password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </FormGroup>
+            {loading ? (
+              <Col lg="12" className="text-center">
+                <h5 className="fw-bold">Loading...</h5>
+              </Col>
+            ) : (
+              <Col lg="6" className="m-auto text-center">
+                <h3 className="fw-bold mb-4">Login</h3>
+                <Form className="auth__form" onSubmit={signIn}>
+                  <FormGroup className="form__group">
+                    <input
+                      type="email"
+                      placeholder="Enter you email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </FormGroup>
+                  <FormGroup className="form__group">
+                    <input
+                      type="password"
+                      placeholder="Enter you password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </FormGroup>
 
-                <button type="submit" className="buy__btn auth__btn">
-                  Login
-                </button>
-                <p>
-                  Don't have an account?
-                  <Link to="/signup">Create an account</Link>
-                </p>
-              </Form>
-            </Col>
+                  <button type="submit" className="buy__btn auth__btn">
+                    Login
+                  </button>
+                  <p>
+                    Don't have an account?
+                    <Link to="/signup">Create an account</Link>
+                  </p>
+                </Form>
+              </Col>
+            )}
           </Row>
         </Container>
       </section>
