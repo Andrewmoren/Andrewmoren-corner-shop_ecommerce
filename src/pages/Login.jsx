80212f9a9465b9f1ea0e080d77d7b74eb426d7af
andrewmoren <andrewmoren93@gmail.com>
2022-12-27 11:29:20 +0300

    build login component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,12 +2,34 @@ import React, { useState } from "react";
 import Helmet from "../components/Helmet/Helmet";
 import { Container, Row, Col, Form, FormGroup } from "reactstrap";
 import { Link } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase.config";
+import { toast } from "react-toastify";
 
 import "../styles/login.css";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const signIn = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+
+      console.log(user);
+    } catch (error) {
+      setLoading(false);
+      toast.success(error.message);
+    }
+  };
   return (
     <Helmet title="Login">
       <section>
