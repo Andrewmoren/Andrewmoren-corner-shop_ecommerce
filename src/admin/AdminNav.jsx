40dbcbadb15f644db38cb0dcf73bcab7b3f8fce2
andrewmoren <andrewmoren93@gmail.com>
2022-12-30 22:11:57 +0300

    create admin nav: end

--- src/admin/AdminNav.jsx
+++ src/admin/AdminNav.jsx
@@ -58,14 +58,21 @@ const AdminNav = () => {
         </div>
       </header>
 
-      <section className="admin__menu">
+      <section className="admin__menu p-0">
         <Container>
           <Row>
             <div className="admin__navigation">
               <ul className="admin__menu-list">
                 {admin__nav.map((item, index) => (
                   <li className="admin__menu-item" key={index}>
-                    <NavLink to={item.path}>{item.display}</NavLink>
+                    <NavLink
+                      to={item.path}
+                      className={(navClass) =>
+                        navClass.isActive ? "active__admin-menu" : ""
+                      }
+                    >
+                      {item.display}
+                    </NavLink>
                   </li>
                 ))}
               </ul>
