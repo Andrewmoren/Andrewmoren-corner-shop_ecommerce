37befc34f205507ba224f0a789b323c7beeb6b04
andrewmoren <andrewmoren93@gmail.com>
2022-12-31 00:59:29 +0300

    add useState in addProducts component

--- src/admin/AddProducts.jsx
+++ src/admin/AddProducts.jsx
@@ -2,6 +2,13 @@ import React, { useState } from "react";
 import { Container, Row, Col, Form, FormGroup } from "reactstrap";
 
 const AddProducts = () => {
+  const [enterTitle, setEnterTitle] = useState("");
+  const [enterShortDesc, setEnterShortDesc] = useState("");
+  const [enterDescription, setEnterDescription] = useState("");
+  const [enterCategory, setEnterCategory] = useState("");
+  const [enterPrice, setEnterPrice] = useState("");
+  const [enterProductImg, setEnterProductImg] = useState(null);
+
   return (
     <section>
       <Container>
@@ -11,26 +18,54 @@ const AddProducts = () => {
             <Form>
               <FormGroup className="form__group">
                 <span>Product title</span>
-                <input type="text" placeholder="Double sofa" />
+                <input
+                  type="text"
+                  placeholder="Double sofa"
+                  value={enterTitle}
+                  onClick={(e) => setEnterTitle(e.target.value)}
+                  required
+                />
               </FormGroup>
               <FormGroup className="form__group">
                 <span>Short Description</span>
-                <input type="text" placeholder="other..." />
+                <input
+                  type="text"
+                  placeholder="Other..."
+                  value={enterShortDesc}
+                  onClick={(e) => setEnterShortDesc(e.target.value)}
+                  required
+                />
               </FormGroup>
               <FormGroup className="form__group">
                 <span>Description</span>
-                <input type="text" placeholder="Description..." />
+                <input
+                  type="text"
+                  placeholder="Description..."
+                  value={enterDescription}
+                  onClick={(e) => setEnterDescription(e.target.value)}
+                  required
+                />
               </FormGroup>
 
               <div className="d-flex align-items-center justify-content-between gap-5">
                 <FormGroup className="form__group w-50">
                   <span>Price</span>
-                  <input type="number" placeholder="$100" />
+                  <input
+                    type="number"
+                    placeholder="$100"
+                    value={enterPrice}
+                    onClick={(e) => setEnterPrice(e.target.value)}
+                    required
+                  />
                 </FormGroup>
 
                 <FormGroup className="form__group w-50">
                   <span>Category</span>
-                  <select className="w-100 p-2">
+                  <select
+                    className="w-100 p-2"
+                    value={enterCategory}
+                    onClick={(e) => setEnterCategory(e.target.value)}
+                  >
                     <option value="chair">Chair</option>
                     <option value="sofa">Sofa</option>
                     <option value="mobile">Mobile</option>
@@ -43,11 +78,13 @@ const AddProducts = () => {
               <div>
                 <FormGroup className="form__group">
                   <span>Product Image</span>
-                  <input type="file" />
+                  <input type="file" required />
                 </FormGroup>
               </div>
 
-              <button className="buy__btn">Add Product</button>
+              <button className="buy__btn" type="submit">
+                Add Product
+              </button>
             </Form>
           </Col>
         </Row>
