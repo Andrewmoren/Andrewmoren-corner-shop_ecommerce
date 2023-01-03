6d0f624f7ba89c55fb7de6aa95571df61d3804dc
andrewmoren <andrewmoren93@gmail.com>
2023-01-03 15:36:10 +0300

    create allProducts component

--- src/admin/AllProducts.jsx
+++ src/admin/AllProducts.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import productImg from "../assets/images/arm-chair-01.jpg";
 
 const AllProducts = () => {
-  return <div>allproducts</div>;
+  return (
+    <section>
+      <Container>
+        <Row>
+          <Col lg="12">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Image</th>
+                  <th>Title</th>
+                  <th>Category</th>
+                  <th>Price</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <img src={productImg} alt="" />
+                  </td>
+                  <td>Arn chair</td>
+                  <td>Chair</td>
+                  <td>$193</td>
+                  <td>
+                    <button className="btn btn-danger">Delete</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 };
 
 export default AllProducts;
