66a5f16de7b0866951c273f24551443709746696
andrewmoren <andrewmoren93@gmail.com>
2023-01-04 20:12:59 +0300

    fix users component

--- src/admin/Users.jsx
+++ src/admin/Users.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
+import useGetData from "../custom-hooks/useGetData";
 
 const Users = () => {
+  const { data: userData, loading } = useGetData("users");
   return (
     <section>
       <Container>
@@ -19,6 +21,25 @@ const Users = () => {
                   <th>Action</th>
                 </tr>
               </thead>
+
+              <tbody>
+                {loading ? (
+                  <h5 className="pt-5 fw-bold">Loading...</h5>
+                ) : (
+                  userData?.map((user) => (
+                    <tr key={user.id}>
+                      <td>
+                        <img src={user.photoURL} alt="photo"></img>
+                      </td>
+                      <td>{user.displayNAme}</td>
+                      <td>{user.email}</td>
+                      <td>
+                        <button className="btn btn-danger">Delete</button>
+                      </td>
+                    </tr>
+                  ))
+                )}
+              </tbody>
             </table>
           </Col>
         </Row>
