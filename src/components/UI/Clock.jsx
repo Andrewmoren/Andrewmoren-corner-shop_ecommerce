cd86c61450cd5f68618a6d46fb4057c4cc8b61de
andrewmoren <andrewmoren93@gmail.com>
2023-03-13 22:08:08 +0300

    fix

--- src/components/UI/Clock.jsx
+++ src/components/UI/Clock.jsx
@@ -11,7 +11,7 @@ const Clock = () => {
   let interval;
 
   const countDown = () => {
-    const destination = new Date("Feb 31, 2023").getTime();
+    const destination = new Date("Feb 31, 2024").getTime();
 
     interval = setInterval(() => {
       const now = new Date().getTime();
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -2,7 +2,7 @@ import React, { useState, useRef, useEffect } from "react";
 
 import { Container, Row, Col } from "reactstrap";
 import { useParams } from "react-router-dom";
-// import products from "../assets/data/products";
+import products from "../assets/data/products";
 import Helmet from "../components/Helmet/Helmet";
 import CommonSection from "../components/UI/CommonSection";
 import { motion } from "framer-motion";
@@ -18,7 +18,7 @@ import { doc, getDoc } from "firebase/firestore";
 import useGetData from "../custom-hooks/useGetData";
 
 const ProductDetails = () => {
-  const [product, setProduct] = useState({});
+  // const [product, setProduct] = useState({});
   const [tab, setTab] = useState("desc");
   const reviewUser = useRef("");
   const reviewMsg = useRef("");
@@ -26,31 +26,32 @@ const ProductDetails = () => {
 
   const [rating, setRating] = useState(null);
   const { id } = useParams();
+  const product = products.find((item) => item.id === id);
 
-  const { data: products } = useGetData("products");
+  // const { data: products } = useGetData("products");
 
   const docRef = doc(db, "products", id);
 
-  useEffect(() => {
-    const getProduct = async () => {
-      const docSnap = await getDoc(docRef);
+  // useEffect(() => {
+  //   const getProduct = async () => {
+  //     const docSnap = await getDoc(docRef);
 
-      if (docSnap.exists()) {
-        setProduct(docSnap.data());
-      } else {
-        console.log("no product!");
-      }
-    };
+  //     if (docSnap.exists()) {
+  //       setProduct(docSnap.data());
+  //     } else {
+  //       console.log("no product!");
+  //     }
+  //   };
 
-    getProduct();
-  }, []);
+  //   getProduct();
+  // }, []);
 
   const {
     imgUrl,
     productName,
     price,
-    // avgRating,
-    // reviews,
+    avgRating,
+    reviews,
     description,
     shortDesc,
     category,
